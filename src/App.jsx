d8c488f3e5976b85805552c55d50080cd75ef5f8
Eric Separovic <ericseparovic07@gmail.com>
2023-02-08 12:24:48 -0300

    Install and config eslint-prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,52 @@
-import { useState, useEffect } from "react";
-import { saveAs } from "file-saver";
+import { useState, useEffect } from 'react';
+import { saveAs } from 'file-saver';
 
 function App() {
-  const [text, setText] = useState("");
-  const [words, setWords] = useState([]);
+	const [text, setText] = useState('');
+	const [words, setWords] = useState([]);
 
-  //Create array
-  const convertToArray = () => {
-    const cleanedText = text.replace(/[^A-Za-z']/g, " ");
-    const words = cleanedText.split(" ");
-    const uniqueWords = [...new Set(words)];
+	// Create array
+	const convertToArray = () => {
+		const cleanedText = text.replace(/[^A-Za-z']/g, ' ');
+		const words = cleanedText.split(' ');
+		const uniqueWords = [...new Set(words)];
 
-    const allWords = uniqueWords.filter((element) => {
-      if (element !== "") {
-        return element;
-      }
-    });
+		const allWords = uniqueWords.filter((element) => element !== '');
 
-    setWords(allWords);
-  };
+		setWords(allWords);
+	};
 
-  // It is responsible for creating the file
-  const createFile = () => {
-    let text = "";
-    words.forEach((word) => {
-      text += `${word} \n`;
-    });
-    const blob = new Blob([text], { type: "text/plain;carhset=utf-8" });
-    saveAs(blob, "words.txt");
-  };
+	// It is responsible for creating the file
+	const createFile = () => {
+		let text = '';
+		words.forEach((word) => {
+			text += `${word} \n`;
+		});
+		const blob = new Blob([text], { type: 'text/plain;carhset=utf-8' });
+		saveAs(blob, 'words.txt');
+	};
 
-  useEffect(() => {
-    convertToArray();
-  }, [text]);
+	useEffect(() => {
+		convertToArray();
+	}, [text]);
 
-  return (
-    <div>
-      <h1>Word List Generator</h1>
-      <form action="">
-        <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-        ></textarea>
-        <button type="button" onClick={createFile}>
-          Create
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Word List Generator</h1>
+			<form action=''>
+				<textarea
+					name=''
+					id=''
+					cols='30'
+					rows='10'
+					onChange={(e) => setText(e.target.value)}
+					value={text}></textarea>
+				<button type='button' onClick={createFile}>
+					Create
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
